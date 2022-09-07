e5adfc83790db1e2021650833c6ada40f8c676e0
AntonStognij <antonstognij5@gmail.com>
2022-09-07 17:13:37 +0300

    1.0.2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,7 @@ import footer from "assets/footer.png"
 
 const Footer = () => {
     return (
-     <footer className="block-footer">
-        <img className = "footer"  src= {footer} alt="footer"/>
-     </footer>
-            
-    
+   <footer>Test</footer>
         
     )
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,15 +6,7 @@ import block from "assets/block.png"
 const Main = () => {
     return (
         <main id="main">
-            <img className = "slider" src= {photo} alt="slider"/>
-            <h2 className="wrap h2">FEATURED PRODUCTS</h2>
-            <div className="wrap catalog">
-           
-            
-            <div>
-            <img className = "block" src= {block} alt="block"/>
-            </div>
-            </div>
+            Main
             
         </main>
         
